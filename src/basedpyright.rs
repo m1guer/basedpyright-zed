0a8f3241a0ccbc6f0e42add2469b93624a32f5a3
Miguerfi <miguelpintojose00@gmail.com>
2024-08-01 13:52:10 -0300

    added support for intialization and workspace configuration

--- src/basedpyright.rs
+++ src/basedpyright.rs
@@ -1,4 +1,4 @@
-use zed_extension_api as zed;
+use zed_extension_api::{self as zed, settings::LspSettings};
 
 struct BasedPyright;
 
@@ -19,6 +19,29 @@ impl zed::Extension for BasedPyright {
             args: vec!["--stdio".to_string(), Default::default()],
             env: Default::default(),
         })
+    }
+    // ref https://github.com/zed-industries/zed/blob/main/extensions/ruff/src/ruff.rs
+    fn language_server_initialization_options(
+        &mut self,
+        language_server_id: &zed_extension_api::LanguageServerId,
+        worktree: &zed_extension_api::Worktree,
+    ) -> zed_extension_api::Result<Option<zed_extension_api::serde_json::Value>> {
+        let settings = LspSettings::for_worktree(language_server_id.as_ref(), worktree)
+            .ok()
+            .and_then(|lsp_settings| lsp_settings.initialization_options.clone())
+            .unwrap_or_default();
+        Ok(Some(settings))
+    }
+    fn language_server_workspace_configuration(
+        &mut self,
+        language_server_id: &zed_extension_api::LanguageServerId,
+        worktree: &zed_extension_api::Worktree,
+    ) -> zed_extension_api::Result<Option<zed_extension_api::serde_json::Value>> {
+        let settings = LspSettings::for_worktree(language_server_id.as_ref(), worktree)
+            .ok()
+            .and_then(|lsp_settings| lsp_settings.settings.clone())
+            .unwrap_or_default();
+        Ok(Some(settings))
     }
 }
 
